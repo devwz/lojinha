da8bbe755e15f3db4c248c4a1f0a8668cc435f0e
devwz <wellington.zaneze@outlook.com.br>
2020-04-28 10:48:33 -0300

    Adiciona Product

---
+++ Src/lojinha/lojinha.Core/Data/Interfaces/ProductRepo.cs
@@ -0,0 +1,41 @@
+﻿using lojinha.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace lojinha.Core.Data.Interfaces
+{
+    public class ProductRepo : GenericRepo<Product>
+    {
+        public ProductRepo(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+
+        public override object Add(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Product> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Product Find(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Src/lojinha/lojinha.Core/Domain/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lojinha.Core.Domain
+{
+    public class Product : Generic
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Bio { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}

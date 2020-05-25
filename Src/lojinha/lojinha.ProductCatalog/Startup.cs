a7cce5c2896cfc75fd52cf579d4718ac381f7fde
devwz <wellington.zaneze@outlook.com.br>
2020-05-25 14:44:49 -0300

    Alteração em lojinha.ProductCatalog

--- Src/lojinha/lojinha.ProductCatalog/Startup.cs
+++ Src/lojinha/lojinha.ProductCatalog/Startup.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using lojinha.Core.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;

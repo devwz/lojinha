bd18ef0c752bc0c09094bc7f5c4cf7c19ea854b0
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2020-07-17 01:19:24 -0300

    Checkout In Progress

--- Src/lojinhaClient/src/app/checkout/checkout.component.ts
+++ Src/lojinhaClient/src/app/checkout/checkout.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart/cart.service';
+import { Cart } from '../cart/cart';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styles: [
-  ]
+  ],
+  providers: [ CartService ]
 })
 export class CheckoutComponent implements OnInit {
+  cart: Cart;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.getCart();
+  }
+
+  getCart(): void {
+    this.cartService.getCart()
+      .subscribe(cart => {
+        this.cart = cart;
+      })
   }
 
 }
